61ad45933f8948771eb2f5abcadc572ba3de8ae3
huangxueliang <huangxueliang@lbdfun.com>
2020-05-22 13:07:23 +0800

    feat:测试defineReactive函数

---
+++ hvue.js
@@ -0,0 +1,29 @@
+class HVue {
+  constructor(options) {
+    this.$options = options
+    this.$data = options.data
+
+    this.observe(this.$data)
+  }
+  observe(data) {
+    if (!data || typeof data !== 'object') {
+      return
+    }
+    Object.keys(data).forEach(key => {
+      this.defineReactive(data, key, data[key])
+    })
+  }
+  defineReactive(obj, key, val) {
+    Object.defineProperty(obj, key, {
+      get() {
+        return val;
+      },
+      set(newVal) {
+        if (newVal !== val) {
+          val = newVal
+          console.log(`${key},更解了：${val}`);
+        }
+      }
+    })
+  }
+}
